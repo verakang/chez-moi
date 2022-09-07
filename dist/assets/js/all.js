7212bd45500dad8eec1dc786d5bf1909d6eef7d8
Vera <sk70885@hotmail.com>
2022-09-07 23:11:54 +0800

    index done

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,6 +1,42 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+  $(function () {
+    $(window).scroll(function () {
+      var scrollVal = $(this).scrollTop();
+
+      if (scrollVal > 400) {
+        $('.gotop').removeClass('d-none');
+      } else {
+        $('.gotop').addClass('d-none');
+      }
+    });
+  });
+
+  (function () {
+    'use strict'; // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
+    var forms = document.querySelectorAll('.needs-validation'); // Loop over them and prevent submission
+
+    Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
 });
 //# sourceMappingURL=all.js.map
